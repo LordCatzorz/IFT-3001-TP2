c986a79011a44203e0653e50041d79a2e2b5f2ed
Lord Catzorz <lordcatzorz@users.noreply.github.com>
2019-04-04 19:15:36 -0400

    Tentative de solution pour q3 n

--- src/question3/ordonnancement.cpp
+++ src/question3/ordonnancement.cpp
@@ -12,11 +12,15 @@ class Travail
     uint numero;
     uint jourAnnonce;
     uint jourDu;
+    Travail* nextNode;
+    Travail* previousNode;
   public:
     Travail(uint _numero, uint _jourAnnonce, uint _jourDu);
     uint GetNumero() const;
     uint GetJourAnnonce() const;
     uint GetJourDu() const;
+    void LinkAfter(Travail* newPreviousNode);
+    void LinkBefore(Travail* newPreviousNode);
 };
 
 Travail::Travail(uint _numero, uint _jourAnnonce, uint _jourDu)
@@ -24,6 +28,8 @@ Travail::Travail(uint _numero, uint _jourAnnonce, uint _jourDu)
   this->numero = _numero;
   this->jourAnnonce = _jourAnnonce;
   this->jourDu = _jourDu;
+  this->nextNode = nullptr;
+  this->previousNode = nullptr;
 }
 
 uint Travail::GetNumero() const
@@ -39,6 +45,25 @@ uint Travail::GetJourAnnonce() const
 uint Travail::GetJourDu() const
 {
   return this->jourDu;
+}
+void Travail::LinkAfter(Travail* newPreviousNode)
+{
+  if (this->previousNode != nullptr)
+  {
+    this->previousNode->nextNode = newPreviousNode;
+  }
+  newPreviousNode->previousNode = this->previousNode;
+  this->previousNode = newPreviousNode;
+}
+
+void Travail::LinkBefore(Travail* newNextNode)
+{
+  if (this->nextNode != nullptr)
+  {
+    this->nextNode->previousNode = newNextNode;
+  }
+  newNextNode->nextNode = this->nextNode;
+  this->nextNode = newNextNode;
 }
 
 /*
@@ -56,28 +81,28 @@ void triParDenombrement(std::vector<Travail*>& S_solution, const uint l_lowerBou
   // Initialisation
   for(uint j = 0; j < rangeMax; j++) // Theta(n)
   {
-    D_distribution[j] = 0;
+    D_distribution.at(j) = 0;
   }
 
   //Fréquence
   for (uint i = 0; i < rangeMax; i++) //Theta(n) si fctCompare in Theta(1)
   {
-    D_distribution[fctCompare(A_elements[i]) - l_lowerBound]++;
+    D_distribution[fctCompare(A_elements.at(i)) - l_lowerBound]++;
   }
 
   //Distribution
   for(uint j = 1; j < rangeMax; j++) //Theta(n)
   {
-    D_distribution[j] = D_distribution[j-1] + D_distribution[j];
+    D_distribution.at(j) = D_distribution.at(j-1) + D_distribution.at(j);
   }
 
   S_solution.resize(rangeMax); //Theta(n)
   //Solution
   for(int i = rangeMax - 1; i >= 0; i--) //Theta(n) is fctCompare in Theta(1)
   {
-    uint j = fctCompare(A_elements[i]) - l_lowerBound;
-    D_distribution[j]--;
-    S_solution[D_distribution[j]] = A_elements[i];
+    uint j = fctCompare(A_elements.at(i)) - l_lowerBound;
+    D_distribution.at(j)--;
+    S_solution.at(D_distribution.at(j)) = A_elements.at(i);
   }
 }
 
@@ -99,18 +124,47 @@ void ordonnancement(const std::vector<unsigned int>& temps_sortie, const std::ve
   std::vector<Travail*> travaux;
   for(uint i = 0; i < nb_travaux; i++)
   {
-    travaux.emplace_back(new Travail(i, temps_sortie[i], echeances[i]));
+    travaux.emplace_back(new Travail(i, temps_sortie.at(i), echeances.at(i)));
   }
   std::function<uint(const Travail*)> f1 = [] (const Travail* t) { return t->GetJourDu();};
   std::function<uint(const Travail*)> f2 = [] (const Travail* t) { return t->GetJourAnnonce();};
 
   std::vector<Travail*> sol;
+  std::vector<Travail*> sol2;
   triParDenombrement(sol, 0, nb_travaux - 1, travaux, f1);
+  triParDenombrement(sol2, 0, nb_travaux - 1, sol, f2);
+
+  std::vector<Travail*> sol3;
+
+  std::vector<int> pos;
+  for(uint i = 0; i < nb_travaux; i++)
+  {
+    pos.push_back(-1);
+  }
+
+  for(uint i = 0; i < nb_travaux; i++)
+  {
+    if (pos.at(sol.at(i)->GetJourAnnonce()) == -1)
+    {
+      pos.at(sol.at(i)->GetJourAnnonce()) = i;
+    }
+  }
+
+  uint currentValue = -1;
+  for(uint i = 0; i < nb_travaux; i++)
+  {
+    if (pos.at(i) == -1)
+    {
+      pos.at(i) = currentValue;
+    } else {
+      currentValue = pos.at(i);
+    }
+  }
 
   solution.resize(nb_travaux);
 
   for(uint i = 0; i < nb_travaux; i++)
   {
-    solution.at(sol[i]->GetNumero()) = i;
+    solution.at(sol.at(i)->GetNumero()) = i;
   }
 }
