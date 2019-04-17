b2c75339c847e30a0e3dd55e810ae2f8860374d9
Lord Catzorz <lordcatzorz@users.noreply.github.com>
2019-04-17 18:57:22 -0400

    réduction en code

--- src/question4/reduction.cpp
+++ src/question4/reduction.cpp
@@ -8,5 +8,11 @@ using namespace std;
 //      grapheG1: G1 est passé en référence et vous devez le modifier pour produire l'output.
 //      grapheG2  G2 est passé en référence et vous devez le modifier pour produire l'output.
 void reduction(const Graphe& grapheCycleHamiltonien, Graphe& grapheG1, Graphe& grapheG2) {
-    //Insérez votre code ici
+  for(size_t i = 0; i < grapheCycleHamiltonien.nbSommets; i++)
+  {
+    grapheG1.arretes.emplace_back(pair<size_t, size_t>(i, (i+1)%grapheCycleHamiltonien.nbSommets));
+  }
+  grapheG1.nbSommets = grapheCycleHamiltonien.nbSommets;
+  
+  grapheG2 = Graphe(grapheCycleHamiltonien);
 }
