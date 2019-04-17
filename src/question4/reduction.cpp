39e21f6c81e337037e3a10b751508884af5e21a1
Lord Catzorz <lordcatzorz@users.noreply.github.com>
2019-04-17 19:09:35 -0400

    Nothing

--- src/question4/reduction.cpp
+++ src/question4/reduction.cpp
@@ -13,6 +13,6 @@ void reduction(const Graphe& grapheCycleHamiltonien, Graphe& grapheG1, Graphe& g
     grapheG1.arretes.emplace_back(pair<size_t, size_t>(i, (i+1)%grapheCycleHamiltonien.nbSommets));
   }
   grapheG1.nbSommets = grapheCycleHamiltonien.nbSommets;
-  
+
   grapheG2 = Graphe(grapheCycleHamiltonien);
 }
